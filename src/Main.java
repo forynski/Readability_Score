59ae2fd1584f9a30da43328e59e4104f69982f76
forynski <65871510+forynski@users.noreply.github.com>
2020-10-09 00:46:13 +0100

    Update Main.java
    1.1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
 
-        if (text.length() > 100) {
+//        if (text.length() > 100) {
+//            System.out.println("HARD");
+//        } else {
+//            System.out.println("EASY");
+//        }
+
+        int counter = (text.split("(([a-zA-Z0-9]([-][_])*[a-zA-Z0-9])+)", -1).length
+                + text.replaceAll("([[a-z][A-Z][0-9][\\W][-][_]]*)", "").length() - 1);
+
+        if (counter > 10) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
