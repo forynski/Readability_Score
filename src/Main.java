7018aec90e622d24bffc99723f141a7808d03e15
forynski <65871510+forynski@users.noreply.github.com>
2020-10-09 03:19:31 +0100

    Stage 2/4 version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,16 @@
 package readability;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        String[] sentences = new Scanner(System.in).nextLine().split("[.!?]");
+        int linesCounter = sentences.length;
+        int words = 0;
 
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-
-        Pattern pattern = Pattern.compile("[\\w-]+");
-        Matcher matcher = pattern.matcher(text);
-        int counter = 0;
-        while (matcher.find())
-            counter++;
-
-        char char1 = '.';
-        char char2 = '!';
-        char char3 = '?';
-
-        int symbolCounter = 0;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == char1 || text.charAt(i) == char2 || text.charAt(i) == char3) {
-                symbolCounter++;
-            }
+        for (String sentence : sentences) {
+            words += sentence.split("\\s+").length;
         }
-
-        double doubleCounter = counter;
-        double doubleSymbolCounter = symbolCounter;
-        double finalCounter = doubleCounter / doubleSymbolCounter;
-
-        if (finalCounter > 10) {
-            System.out.println("HARD");
-        } else {
-            System.out.println("EASY");
-
-        }
+        System.out.println(words/linesCounter <= 10 ? "EASY" : "HARD");
     }
 }
-
