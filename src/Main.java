3b327b28b24664632cdcacd21542067128a851a8
forynski <65871510+forynski@users.noreply.github.com>
2020-10-09 03:04:04 +0100

    Work on project. Stage 2/4: Words and sentences

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 package readability;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,20 +10,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
 
-//        if (text.length() > 100) {
-//            System.out.println("HARD");
-//        } else {
-//            System.out.println("EASY");
-//        }
+        Pattern pattern = Pattern.compile("[\\w-]+");
+        Matcher matcher = pattern.matcher(text);
+        int counter = 0;
+        while (matcher.find())
+            counter++;
 
-        int counter = (text.split("(([a-zA-Z0-9]([-][_])*[a-zA-Z0-9])+)", -1).length
-                + text.replaceAll("([[a-z][A-Z][0-9][\\W][-][_]]*)", "").length() - 1);
+        char char1 = '.';
+        char char2 = '!';
+        char char3 = '?';
 
-        if (counter > 10) {
+        int symbolCounter = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == char1 || text.charAt(i) == char2 || text.charAt(i) == char3) {
+                symbolCounter++;
+            }
+        }
+
+        double doubleCounter = counter;
+        double doubleSymbolCounter = symbolCounter;
+        double finalCounter = doubleCounter / doubleSymbolCounter;
+
+        if (finalCounter > 10) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
-        }
 
+        }
     }
 }
+
