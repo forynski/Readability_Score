ace87ac43d8063b94e0a1575f7b3fb41415dcfe8
forynski <65871510+forynski@users.noreply.github.com>
2020-10-13 18:44:39 +0100

    stage 3/4 update
    added switch instruction

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,10 @@ public class Main {
         System.out.println(text);
 
         // words counter
-        int words = text.split("\\w+").length ;
+//        int words = text.split("\\w+").length ;
+//        int words = text.split("\\w+").length ;
+        int words = text.replaceAll("[0-9]", "").split("\\w+").length ;
+
         System.out.println("Words: " + words);
 
         // sentences counter
@@ -32,8 +35,54 @@ public class Main {
         double score = 4.71 * ((double) characters / (double) words)
                 + 0.5 * ((double) words / (double) sentences) - 21.43;
         System.out.printf("The score is: %.2f", score);
+        System.out.println();
+        int scoreRounded = (int) Math.ceil(score);
 
-        
+        switch (scoreRounded) {
+            case 1:
+                System.out.println("This text should be understood by 5-6 year olds.");
+                break;
+            case 2:
+                System.out.println("This text should be understood by 6-7 year olds.");
+                break;
+            case 3:
+                System.out.println("This text should be understood by 7-9 year olds.");
+                break;
+            case 4:
+                System.out.println("This text should be understood by 9-10 year olds.");
+                break;
+            case 5:
+                System.out.println("This text should be understood by 10-11 year olds.");
+                break;
+            case 6:
+                System.out.println("This text should be understood by 11-12 year olds.");
+                break;
+            case 7:
+                System.out.println("This text should be understood by 12-13 year olds.");
+                break;
+            case 8:
+                System.out.println("This text should be understood by 13-14 year olds.");
+                break;
+            case 9:
+                System.out.println("This text should be understood by 14-15 year olds.");
+                break;
+            case 10:
+                System.out.println("This text should be understood by 15-16 year olds.");
+                break;
+            case 11:
+                System.out.println("This text should be understood by 16-17 year olds.");
+                break;
+            case 12:
+                System.out.println("This text should be understood by 17-18 year olds.");
+                break;
+            case 13:
+                System.out.println("This text should be understood by 18-24 year olds.");
+                break;
+            case 14:
+                System.out.println("This text should be understood by 24+ year olds.");
+                break;
+        }
+
 
     }
 
@@ -42,4 +91,3 @@ public class Main {
     }
 
 }
-
